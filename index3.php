404d5b4062ffe9def0a23a7b929cc570eb80e54f
Khieran <100107960+Khieran@users.noreply.github.com>
2022-02-25 21:51:07 +0800

    Update index3.php

--- index3.php
+++ index3.php
@@ -37,7 +37,7 @@
 <hr>
 <center>
 <table border 5 bgcolor = lightgray width 50%>
-<th> <a href = "Mainpage.html"> Return To Homepage </th>
+<th> <a href = "index.php"> Return To Homepage </th>
 </table>
 </center>
 </body>
